2f2d69516d6dc1bac8e0dd90a0fe606459d9b6cb
Michael Gregory <michael.gregory8584@gmail.com>
2020-05-08 12:53:24 -0500

    mvP

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -16,13 +16,8 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  // const [values, setValues] = useState(initialValue);
+
   const [values, handleChanges] = useForm(initialValue);
-
-
-  // const handleChanges = (e) => {
-  //   setValues({ ...values, [e.target.name]: e.target.value });
-  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -69,7 +64,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkout-button">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByText, fireEvent, getByLabelText, findByText } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />)
+    const header = getByText(/checkout form/i)
 
-test("form shows success message on submit with form details", () => {});
+    expect(header).toBeInTheDocument()
+});
+
+test("form shows success message on submit with form details", () => {
+    const { getByLabelText, getByTestId } = render(<CheckoutForm />)
+
+    const successMessage = findByText(/You have ordered some plants! Woo-hoo!/i)
+
+    fireEvent.change(getByLabelText(/first name/i), { target: {value: "Michael"}})
+    fireEvent.change(getByLabelText(/last name/i), { target: {value: "Gregory"}})
+    fireEvent.change(getByLabelText(/address/i), { target: {value: "14605 34th Ave N"}})
+    fireEvent.change(getByLabelText(/city/i), { target: {value: "Plymouth"}})
+    fireEvent.change(getByLabelText(/state/i), { target: {value: "MN"}})
+    fireEvent.change(getByLabelText(/zip/i), { target: {value: "55447"}})
+    fireEvent.click(getByTestId(/checkout-button/))
+
+    expect(successMessage).toBeInTheDocument
+
+});
