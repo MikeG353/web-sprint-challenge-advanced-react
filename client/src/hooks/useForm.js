6952d030a755648fe65c9f31857cace0c84e556b
Michael Gregory <michael.gregory8584@gmail.com>
2020-05-08 12:02:47 -0500

    added useForm some basic funtionality is up and running checkout button shows shipping info the info updates still after typing

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
+import { useState } from "react";
+
 // write your custom hook here to control your checkout form
+export const useForm = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChanges = (e) => {
+        setValue({ ...value, [e.target.name]: e.target.value });
+      };
+ 
+    return [value, handleChanges]
+}
